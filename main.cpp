bdb5c99f11ca60e093a8ae5e49814140a6610c82
Maël <mael.fortune@gmail.com>
2019-10-06 23:35:51 +0200

    Bronze : 1031

--- main.cpp
+++ main.cpp
@@ -39,19 +39,92 @@ static constexpr int ROBOTS = 5;
 
 int turn = 1;
 
-class Point;
+class Point {
+public:
+    int x{-1};
+    int y{-1};
+    int sx{-1};
+    int sy{-1};
 
-class Cell;
+    Point() = default;
+    Point(int x, int y);
+    virtual void move(int x, int y);
+    int distance(Point *point);
+    virtual void reset();
+};
 
-class Entity;
+class Cell : public Point {
+public:
+    bool hole{false};
+    bool oreVisible{false};
+    int ore{0};
 
-class Robot;
+    Cell() = default;
+    void update(Point *point, bool hole, bool oreVisible, int ore);
+};
 
-class Game;
+class Entity : public Point {
+public:
+    int id{0};
+    Type type{Type::NONE};
+    Type item{Type::NONE};
+    int owner{0};
 
-class Player;
+    Entity() = default;
+    Entity(int id, Type type, Point *point, Type item, int owner);
+    void update(int id, Type type, Point *point, Type item, int owner);
+};
 
-class Action;
+class Action {
+public:
+    string type{"WAIT"};
+    string arguments{""};
+
+    Action() = default;
+};
+
+class Robot : public Entity {
+public:
+    Action *action{};
+    Point *destination{};
+    vector<Point *> destinations;
+
+    Robot();
+    bool isDead();
+    void search();
+    void updateDestination(int x, int y);
+    void takeAction();
+    void printAction();
+};
+
+class Player {
+public:
+    Robot *robots[ROBOTS];
+    int ore{0};
+    int cooldownRadar{0}, cooldownTrap{0};
+    int owner{0};
+
+    Player() = default;
+    Player(int owner);
+    void updateRobot(int id, Point *point, Type item, int owner);
+    void updateOre(int ore);
+    void updateCooldownRadar(int cooldown);
+    void updateCooldownTrap(int cooldown);
+};
+
+class Game {
+public:
+    Cell *grid[WIDTH][HEIGHT];
+    Player *players[PLAYERS];
+    vector<Entity *> radars;
+    vector<Entity *> traps;
+
+    Game();
+    Player *player();
+    Player *opponent();
+    void updateCell(int x, int y, const string &ore, int hole);
+    void updateEntity(int id, int type, int x, int y, int _item);
+};
 
 class Individual;
 
@@ -64,302 +137,263 @@ int random_num(int start, int end) {
     return random_int;
 }
 
-class Point {
-public:
-    int x{-1};
-    int y{-1};
-    int sx{-1};
-    int sy{-1};
+Point::Point(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
 
-    Point() = default;
+void Point::move(int x, int y) {
+    this->x = x;
+    this->y = y;
+}
 
-    Point(int x, int y) {
-        this->x = x;
-        this->y = y;
+int Point::distance(Point *point) {
+    return abs(x - point->x) + abs(y - point->y);
+}
+
+void Point::reset() {
+    this->x = this->sx;
+    this->y = this->sy;
+}
+
+void Cell::update(Point *point, bool hole, bool oreVisible, int ore) {
+    this->hole = hole;
+    this->ore = ore;
+    this->oreVisible = oreVisible;
+    this->x = point->x;
+    this->y = point->y;
+}
+
+Entity::Entity(int id, Type type, Point *point, Type item, int owner) : Point() {
+    this->x = point->x;
+    this->y = point->y;
+    this->id = id;
+    this->type = type;
+    this->owner = owner;
+    this->item = item;
+}
+
+void Entity::update(int id, Type type, Point *point, Type item, int owner) {
+    this->x = point->x;
+    this->y = point->y;
+    this->id = id;
+    this->type = type;
+    this->owner = owner;
+    this->item = item;
+}
+
+Robot::Robot() {
+    this->action = new Action();
+    this->destination = new Point();
+}
+
+bool Robot::isDead() {
+    return this->x == -1 && this->y == -1;
+}
+
+void Robot::search() {
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            if (this->distance(game->grid[x][y])) {
+
+            }
+        }
+    }
+}
+
+void Robot::updateDestination(int x, int y) {
+    this->destination->x = x;
+    this->destination->y = y;
+
+    if (this->destination->x > WIDTH) {
+        this->destination->x = 8;
+        this->destination->y += 1;
     }
 
-    virtual void move(int x, int y) {
-        this->x = x;
-        this->y = y;
+    if (this->destination->x < 0) {
+        this->destination->x = 8;
+        this->destination->y += 1;
     }
 
-    int distance(Point *point) {
-        return abs(x - point->x) + abs(y - point->y);
+    if (this->destination->y > HEIGHT) {
+        this->destination->y = 0;
+        this->destination->x = 8;
     }
 
-    virtual void reset() {
-        this->x = this->sx;
-        this->y = this->sy;
+    if (this->destination->y < 0) {
+        this->destination->y = 0;
     }
-};
+}
 
-class Cell : public Point {
-public:
-    bool hole{false};
+void Robot::takeAction() {
+    // Le premier tour, le robot va au moins à x = 8 pour trouver du cristal.
+    if (turn == 1) {
+        this->action->type = "MOVE";
+        this->destination->x = 8;
+        this->destination->y = this->y;
+        return;
+    }
+
+    // Après avoir livré du cristal au QG, le robot retourne à un endroit où il y a potentiellement du cristal.
+    if (this->x == 0) {
+        this->action->type = "MOVE";
+        this->destination->x = 4 * random_num(1, 7);
+        this->destination->y = this->y;
+        return;
+    }
+
+    // Si le robot est mort, il attend.
+    if (this->isDead()) {
+        this->action->type = "WAIT";
+        return;
+    }
+
+    // Si le robot porte un cristal, il retourne au QG.
+    if (this->item == Type::ORE) {
+        cerr << "Robot " << this->id << " a trouvé un cristal !" << endl;
+        this->action->type = "MOVE";
+        this->destination->x = 0;
+        this->destination->y = this->y;
+        return;
+    }
+
+    if (this->destination->x == this->x && this->destination->y == this->y && this->action->type != "DIG") {
+        this->action->type = "DIG";
+    } else {
+        this->action->type = "MOVE";
+        this->updateDestination(this->destination->x + 1, this->destination->y);
+
+//            switch(random_num(0, 3)) {
+//                case 0:
+//                    this->updateDestination(this->destination->x + 4, this->destination->y);
+//                    break;
+//                case 1:
+//                    this->updateDestination(this->destination->x - 4, this->destination->y);
+//                    break;
+//                case 2:
+//                    this->updateDestination(this->destination->x, this->destination->y + 4);
+//                    break;
+//                case 3:
+//                    this->updateDestination(this->destination->x, this->destination->y - 4);
+//                    break;
+//                default: break;
+//            }
+    }
+}
+
+void Robot::printAction() {
+    cout << this->action->type << " " << this->destination->x << " " << this->destination->y << endl;
+}
+
+Player::Player(int owner) {
+    for (int i = 0; i < ROBOTS; i++) {
+        this->robots[i] = new Robot();
+    }
+
+    this->owner = owner;
+}
+
+void Player::updateRobot(int id, Point *point, Type item, int owner) {
+    int idxOffset{0};
+
+    if (id >= ROBOTS) {
+        idxOffset = ROBOTS;
+    }
+
+    this->robots[id - idxOffset]->update(id, Type::ROBOT, point, item, owner);
+    this->owner = owner;
+}
+
+void Player::updateOre(int ore) {
+    this->ore = ore;
+}
+
+void Player::updateCooldownRadar(int cooldown) {
+    this->cooldownRadar = cooldown;
+}
+
+void Player::updateCooldownTrap(int cooldown) {
+    this->cooldownTrap = cooldown;
+}
+
+Game::Game() {
+    for (int x = 0; x < WIDTH; x++) {
+        for (int y = 0; y < HEIGHT; y++) {
+            this->grid[x][y] = new Cell();
+        }
+    }
+
+    this->players[0] = new Player(0);
+    this->players[1] = new Player(1);
+}
+
+Player* Game::player() {
+    return this->players[0];
+}
+
+Player* Game::opponent() {
+    return this->players[1];
+}
+
+void Game::updateCell(int x, int y, const string &ore, int hole) {
+    int oreAmount{0};
     bool oreVisible{false};
-    int ore{0};
+    Point *point = new Point{x, y};
 
-    Cell() = default;
-
-    void update(Point *point, bool hole, bool oreVisible, int ore) {
-        this->hole = hole;
-        this->ore = ore;
-        this->oreVisible = oreVisible;
-        this->x = point->x;
-        this->y = point->y;
+    if (ore != "?") {
+        oreAmount = stoi(ore);
+        oreVisible = true;
     }
-};
 
-class Action {
-public:
-    string type{"WAIT"};
-    string arguments{""};
+    this->grid[x][y]->update(point, (bool) hole, oreVisible, oreAmount);
+}
 
-    Action() = default;
-};
-
-class Entity : public Point {
-public:
-    int id{0};
-    Type type{Type::NONE};
+void Game::updateEntity(int id, int type, int x, int y, int _item) {
+    // item
     Type item{Type::NONE};
-    int owner{0};
 
-    Entity() = default;
-
-    Entity(int id, Type type, Point *point, Type item, int owner) : Point() {
-        this->x = point->x;
-        this->y = point->y;
-        this->id = id;
-        this->type = type;
-        this->owner = owner;
-        this->item = item;
+    //-1 for NONE, 2 for RADAR, 3 for TRAP, 4 ORE
+    switch (_item) {
+        case -1:
+            item = Type::NONE;
+            break;
+        case 2:
+            item = Type::RADAR;
+            break;
+        case 3:
+            item = Type::TRAP;
+            break;
+        case 4:
+            item = Type::ORE;
+            break;
+        default:
+            break;
     }
 
-    void update(int id, Type type, Point *point, Type item, int owner) {
-        this->x = point->x;
-        this->y = point->y;
-        this->id = id;
-        this->type = type;
-        this->owner = owner;
-        this->item = item;
-    }
-};
+    Point *point = new Point{x, y};
 
-class Robot : public Entity {
-public:
-    Action *action{};
-    Point *destination{};
-
-    Robot() {
-        this->action = new Action();
-        this->destination = new Point();
-    }
-
-    bool isDead() {
-        return this->x == -1 && this->y == -1;
-    }
-
-    void updateDestination(int x, int y) {
-        this->destination->x = x;
-        this->destination->y = y;
-
-        if (this->destination->x > WIDTH) {
-            this->destination->x = WIDTH;
+    // 0 for your robot, 1 for other robot, 2 for radar, 3 for trap
+    switch (type) {
+        case 0:
+            this->player()->updateRobot(id, point, item, type);
+            break;
+        case 1:
+            this->opponent()->updateRobot(id, point, item, type);
+            break;
+        case 2: {
+            Entity *radar = new Entity(id, Type::RADAR, point, item, 0);
+            this->radars.emplace_back(radar);
+            break;
         }
-
-        if (this->destination->x < 0) {
-            this->destination->x = 0;
+        case 3: {
+            Entity *trap = new Entity(id, Type::TRAP, point, item, 0);
+            this->traps.emplace_back(trap);
+            break;
         }
-
-        if (this->destination->y > HEIGHT) {
-            this->destination->y = HEIGHT;
-        }
-
-        if (this->destination->y < 0) {
-            this->destination->y = 0;
-        }
+        default:
+            break;
     }
-
-    void takeAction() {
-        // Le premier tour, les robots vont au moins à x = 8 pour trouver du cristal.
-        if (this->x == 0) {
-            this->action->type = "MOVE";
-            this->destination->x = 4 * random_num(1, 7);
-            this->destination->y = this->y;
-            return;
-        }
-
-        // Si le robot est mort, il attend.
-        if (this->isDead()) {
-            this->action->type = "WAIT";
-            return;
-        }
-
-        // Si le robot porte un cristal, il retourne au QG.
-        if (this->item == Type::ORE) {
-            cerr << "Robot " << this->id << " a trouvé un crsital !" << endl;
-            this->action->type = "MOVE";
-            this->destination->x = 0;
-            this->destination->y = this->y;
-            return;
-        }
-
-        if (this->destination->x == this->x && this->destination->y == this->y && this->action->type != "DIG") {
-            this->action->type = "DIG";
-        } else {
-            this->action->type = "MOVE";
-
-            switch(random_num(0, 3)) {
-                case 0:
-                    this->updateDestination(this->destination->x + 4, this->destination->y);
-                    break;
-                case 1:
-                    this->updateDestination(this->destination->x - 4, this->destination->y);
-                    break;
-                case 2:
-                    this->updateDestination(this->destination->x, this->destination->y + 4);
-                    break;
-                case 3:
-                    this->updateDestination(this->destination->x, this->destination->y - 4);
-                    break;
-                default: break;
-            }
-        }
-    }
-
-    void printAction() {
-        cout << this->action->type << " " << this->destination->x << " " << this->destination->y << endl;
-    }
-};
-
-class Player {
-public:
-    Robot *robots[ROBOTS];
-    int ore{0};
-    int cooldownRadar{0}, cooldownTrap{0};
-    int owner{0};
-
-    Player() = default;
-
-    Player(int owner) {
-        for (int i = 0; i < ROBOTS; i++) {
-            this->robots[i] = new Robot();
-        }
-
-        this->owner = owner;
-    }
-
-    void updateRobot(int id, Point *point, Type item, int owner) {
-        int idxOffset{0};
-
-        if (id >= ROBOTS) {
-            idxOffset = ROBOTS;
-        }
-
-        this->robots[id - idxOffset]->update(id, Type::ROBOT, point, item, owner);
-        this->owner = owner;
-    }
-
-    void updateOre(int ore) {
-        this->ore = ore;
-    }
-
-    void updateCooldownRadar(int cooldown) {
-        this->cooldownRadar = cooldown;
-    }
-
-    void updateCooldownTrap(int cooldown) {
-        this->cooldownTrap = cooldown;
-    }
-};
-
-class Game {
-public:
-    Cell *grid[WIDTH][HEIGHT];
-    Player *players[PLAYERS];
-    vector<Entity *> radars;
-    vector<Entity *> traps;
-
-    Game() {
-        for (int x = 0; x < WIDTH; x++) {
-            for (int y = 0; y < HEIGHT; y++) {
-                this->grid[x][y] = new Cell();
-            }
-        }
-
-        this->players[0] = new Player(0);
-        this->players[1] = new Player(1);
-    }
-
-    Player *player() {
-        return this->players[0];
-    }
-
-    Player *opponent() {
-        return this->players[1];
-    }
-
-    void updateCell(int x, int y, const string &ore, int hole) {
-        int oreAmount{0};
-        bool oreVisible{false};
-        Point *point = new Point{x, y};
-
-        if (ore != "?") {
-            oreAmount = stoi(ore);
-            oreVisible = true;
-        }
-
-        this->grid[x][y]->update(point, (bool) hole, oreVisible, oreAmount);
-    }
-
-    void updateEntity(int id, int type, int x, int y, int _item) {
-        // item
-        Type item{Type::NONE};
-
-        //-1 for NONE, 2 for RADAR, 3 for TRAP, 4 ORE
-        switch (_item) {
-            case -1:
-                item = Type::NONE;
-                break;
-            case 2:
-                item = Type::RADAR;
-                break;
-            case 3:
-                item = Type::TRAP;
-                break;
-            case 4:
-                item = Type::ORE;
-                break;
-            default:
-                break;
-        }
-
-        Point *point = new Point{x, y};
-
-        // 0 for your robot, 1 for other robot, 2 for radar, 3 for trap
-        switch (type) {
-            case 0:
-                this->player()->updateRobot(id, point, item, type);
-                break;
-            case 1:
-                this->opponent()->updateRobot(id, point, item, type);
-                break;
-            case 2: {
-                Entity *radar = new Entity(id, Type::RADAR, point, item, 0);
-                this->radars.emplace_back(radar);
-                break;
-            }
-            case 3: {
-                Entity *trap = new Entity(id, Type::TRAP, point, item, 0);
-                this->traps.emplace_back(trap);
-                break;
-            }
-            default:
-                break;
-        }
-    }
-};
+}
 
 /**
  * Deliver more ore to hq (left side of the map) than your opponent. Use radars to find ore but beware of traps!
